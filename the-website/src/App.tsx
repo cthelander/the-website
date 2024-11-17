e1a0f82bc741576c32fb8895639d272a1a447de3
Carly Thelander <cthelander@Carlys-MacBook-Pro.local>
2024-11-17 15:18:37 -0800

    adding heroku

--- the-website/src/App.tsx
+++ the-website/src/App.tsx
@@ -5,10 +5,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-  Helllo world {count}
-  <button onClick={() => setCount(count +1 )} >add</button>
-    </>
+    <div>
+      Welcome to project Carly!
+
+
+      <button onClick={() => setCount(count +1 )} >add</button>
+
+      <img src="./Instagram_icon.png"/>
+      {count}
+    </div>
   )
 }
 
