03dcd83ddbe698c737d1f209fe1a2900b5253753
Carly Thelander <cthelander@Carlys-MacBook-Pro.local>
2025-01-12 17:35:51 -0800

    hover styles

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,19 @@
 import { ReactElement } from "react";
+import styled from "styled-components";
 
+const LinkContainer = styled.a`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 6px 6px 0 6px;
+  &:hover {
+    border-color: #646cff;
+  }
+`
 export function Link({ href, icon }: { href: string, icon: ReactElement }) {
 
   return (
-    <a href={href} target="_blank">
+    <LinkContainer href={href} target="_blank">
       {icon}
-    </a>
+    </LinkContainer>
   )
 }
